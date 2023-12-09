77b40eb3f4d47575ff22f89511bc7b675071cf2c
Prasetyono Putra <prasetyonoptr@gmail.com>
2023-12-09 15:39:27 +0700

    Test 2

--- src/components/note-component/NoteComponent.js
+++ src/components/note-component/NoteComponent.js
@@ -3,6 +3,7 @@ import optionLogo from '../../icons/icon-option.png';
 
 
 function NoteComponent() {
+    console.log("Test 1");
     return (
         <div className="note-container">
             <img src={optionLogo} height="8px" width="30px" className="note-option"/>
